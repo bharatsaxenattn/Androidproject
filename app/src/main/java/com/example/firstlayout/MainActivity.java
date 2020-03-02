46b4a30880983907b2f4d14ebf60af5581d31405
bharat <bharat@tothenew.com>
2020-03-02 16:11:24 +0530

    recycler view with pagination and news page only

--- app/src/main/java/com/example/firstlayout/MainActivity.java
+++ app/src/main/java/com/example/firstlayout/MainActivity.java
@@ -9,16 +9,47 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button signinButton;
+    Button signinButton,signinButton1,recycler1,recycler2,signUpBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        signinButton=findViewById(R.id.signin);
+        signinButton=findViewById(R.id.layout_design);
+        signinButton1=findViewById(R.id.layout_design1);
+        signUpBtn=findViewById(R.id.layout_design2);
+        recycler1=findViewById(R.id.recyclerBtn);
+        recycler2=findViewById(R.id.recyclerBtn1);
         signinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,Restaurnats.class));
+            }
+        });
+        signinButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Restaurant1.class));
+
+            }
+        });
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUp.class));
+
+            }
+        });
+        recycler1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,PaginationActivity.class));
+            }
+        });
+        recycler2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CustomRecyclerview.class));
+
             }
         });
     }
