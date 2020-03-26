412f7761756dabd4296558e870fd33fa7a2c5891
bharat <bharat@tothenew.com>
2020-03-26 16:41:09 +0530

    login module in kotlin added

---
+++ loginmodule/src/main/java/com/example/loginmodule/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.loginmodule
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val intent=Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
